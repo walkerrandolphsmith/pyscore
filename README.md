e9ac34056d5c96cabe7ea6cc9d03fa960244ffa3
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-10-31 20:31:33 -0400

    Write spec for reverse

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # PyScore
 
 Collection of utility functions in Python inspired by Underscore and Lodash
+
+Virtual Environment
+virtualenv venv --no-site-packages
+
+Install packages
+./venv/bin/pip install -r requirements.txt
+
+Run specs
+./venv/bin/nosetests test
---
+++ test/test_reverse.py
@@ -0,0 +1,12 @@
+import unittest
+from _ import reverse
+
+class sut(unittest.TestCase):
+
+    def setUp(self):
+        self.reverse = reverse.reverse
+
+    def test_reverse_reverses_a_list(self):
+        expected = [3, 2, 1]
+        actual = self.reverse([1, 2, 3])
+        self.assertEqual(expected, actual)

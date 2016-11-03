34ec4ef3c004e1556df706c87c234f7622be0aeb
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-11-02 21:08:19 -0400

    Unique

---
+++ test/test_unique.py
@@ -0,0 +1,13 @@
+import unittest
+from _ import unique
+
+class sut(unittest.TestCase):
+
+    def setUp(self):
+
+        self.unique = unique.unique
+
+    def test_unique_returns_all_elements_of_a_and_b(self):
+        expected = [1, 2]
+        actual = self.unique([1, 1, 2])
+        self.assertEqual(expected, actual)

f615bd901ca7c186050c4b6e5b1c16686d92cb70
agguzman <a.guillermoguzman@gmail.com>
2016-11-09 22:36:37 -0500

    pluck returns a list of the extracted property values

---
+++ test/test_pluck.py
@@ -0,0 +1,15 @@
+import unittest
+from _ import pluck
+
+class sut(unittest.TestCase):
+
+    def setUp(self):
+        self.pluck = pluck.pluck
+
+    def test_some_returns_true_if_any_element_satisfies_predicate(self):
+        first = {"id": 1, "size": "M"}
+        second = {"id": 2, "size": "M"}
+        third = {"id": 3, "size": "L"}
+        expected = [1, 2, 3]
+        actual = self.pluck([first, second, third], 'id')
+        self.assertEqual(expected, actual)

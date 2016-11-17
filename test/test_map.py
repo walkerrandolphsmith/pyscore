bd05c6cb41b3ef379545840f0a246aa2bafe6248
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-11-16 23:05:49 -0500

    Pep8 blank lines.

--- test/test_map.py
+++ test/test_map.py
@@ -11,7 +11,6 @@ class sut(unittest.TestCase):
         actual = self.map([1, 2, 3, 4], lambda e, i: e * 2 + i)
         self.assertEqual(expected, actual)
 
-
     def test_map_iteratee_has_one_arg(self):
         expected = [2, 4, 6, 8]
         actual = self.map([1, 2, 3, 4], lambda e: e * 2)

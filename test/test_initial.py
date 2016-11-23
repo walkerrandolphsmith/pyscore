17fc2ce36e9ac60e80424732e6abc9610b0b8454
agguzman <a.guillermoguzman@gmail.com>
2016-11-22 20:35:26 -0500

    error in naming, rename test

--- test/test_initial.py
+++ test/test_initial.py
@@ -7,12 +7,12 @@ class Sut(unittest.TestCase):
     def setUp(self):
         self.sut = initial
 
-    def test_last_returns_the_last_element_in_collection(self):
+    def test_initial_returns_all_except_last_element(self):
         expected = [5, 4, 3, 2]
         actual = self.sut([5, 4, 3, 2, 1])
         self.assertEqual(expected, actual)
 
-    def test_last_returns_the_last_n_elements_in_collection(self):
+    def test_initial_returns_all_except_last_n_elements(self):
         expected = [5, 4]
         actual = self.sut([5, 4, 3, 2, 1], 3)
         self.assertEqual(expected, actual)
